add9f9237b5b6954d0f40fad9491533275bdc497
Sagar Chamling <sgr.raee@gmail.com>
2017-04-26 12:33:25 +0545

    Update migration file

--- database/migrations/2014_01_07_073615_create_tags_table.php
+++ database/migrations/2014_01_07_073615_create_tags_table.php
@@ -2,22 +2,23 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTagsTable extends Migration {
 
-	public function up()
-	{
-		Schema::create('tagging_tags', function(Blueprint $table) {
-			$table->increments('id');
-			$table->string('slug', 255)->index();
-			$table->string('name', 255);
-			$table->boolean('suggest')->default(false);
-			$table->integer('count')->unsigned()->default(0); // count of how many times this tag was used
-		});
-	}
+    public function up()
+    {
+        Schema::create('tagging_tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug', 255)->index();
+            $table->string('name', 255);
+            $table->boolean('suggest')->default(false);
+            $table->integer('count')->unsigned()->default(0); // count of how many times this tag was used
+        });
+    }
 
-	public function down()
-	{
-		Schema::drop('tagging_tags');
-	}
+    public function down()
+    {
+        Schema::dropIfExists('tagging_tags');
+    }
 }
