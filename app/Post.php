5725b9c3ce61720bf2dc5c13607c1e9db0d70802
Sagar Chamling <sgr.raee@gmail.com>
2017-04-26 12:38:28 +0545

    Add taggable traits and fillable field

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use Conner\Tagging\Taggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use Taggable;
+
+    protected $fillable = [
+        'title', 'body'
+    ];
 }
