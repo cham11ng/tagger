4a971ade05a3261a2d26db9a475fdeeb199ae870
Sagar Chamling <sgr.raee@gmail.com>
2017-04-26 15:16:36 +0545

    Create view for post

---
+++ resources/views/create.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row" style="padding-top: 100px;">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Post</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ route('posts.store') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                                <label for="title" class="col-md-4 control-label">Title</label>
+
+                                <div class="col-md-6">
+                                    <input id="title" type="text" class="form-control" name="title"
+                                           value="{{ old('title') }}" required autofocus>
+
+                                    @if ($errors->has('title'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
+                                <label for="body" class="col-md-4 control-label">Body</label>
+
+                                <div class="col-md-6">
+                                    <textarea id="body" class="form-control" name="body" required></textarea>
+
+                                    @if ($errors->has('body'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('body') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('tags') ? ' has-error' : '' }}">
+                                <label for="tags" class="col-md-4 control-label">Tags</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" name="tags" id="tags" class="form-control">
+
+                                    @if ($errors->has('tags'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('tags') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        var tags = [
+            @foreach ($tags as $tag){
+                tag: "{{ $tag }}"
+            },
+            @endforeach
+        ];
+    </script>
+
+@endsection
